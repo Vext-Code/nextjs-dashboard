156e06f56e11ebbc9ecad13a71ca801b975e8e85
Fanfan Rudi Afandi <fanfan@Fanfans-MacBook-Pro.local>
2025-03-26 20:30:28 +0700

    Initial commit from Create Next App

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import '@/app/ui/global.css';
+import { inter, nunitoSans, lusitana } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${nunitoSans.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Nunito_Sans, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const nunitoSans = Nunito_Sans({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
